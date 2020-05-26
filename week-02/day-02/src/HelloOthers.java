7c78cf56670c5b81952fd2fae8df4fd0b4bc902f
Martin Fučík <mar.fucik@gmail.com>
2020-05-27 01:04:59 +0200

    HelloOthers exercise

---
+++ week-02/day-02/src/HelloOthers.java
@@ -0,0 +1,12 @@
+public class HelloOthers {
+  public static void main(String[] args) {
+    System.out.println("Hello, Jirina!");
+    System.out.println("Hello, Amy!");
+    System.out.println("Hello, Andrea!");
+    System.out.println("Hello, Jitka!");
+    System.out.println("Hello, Jakub!");
+    System.out.println("Hello, Tomas!");
+    System.out.println("Hello, Marek!");
+    System.out.println("Hello, Michael!");
+  }
+}
