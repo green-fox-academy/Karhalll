bebf53d642f067f07d8cff1f70b75356591e2273
Martin Fučík <mar.fucik@gmail.com>
2020-05-27 12:44:40 +0200

    DefinebasicInfo exercise

---
+++ week-02/day-03/src/Variables/Exercises/DefineBasicInfo.java
@@ -0,0 +1,21 @@
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    // Define several things as a variable then print their values
+    // Your name as a string
+    String name = "Martin Fučík";
+
+    // Your age as an integer
+    int age = 30;
+
+    // Your height in meters as a double
+    double height = 1.95;
+
+    // Whether you are married or not as a boolean
+    boolean married = false;
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Height: " + height);
+    System.out.println("Married: " + married);
+  }
+}
