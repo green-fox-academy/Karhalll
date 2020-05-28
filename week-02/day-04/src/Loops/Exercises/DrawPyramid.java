e4897d081e5e1227c521c9a67bcbd8c679e7c646
Martin Fučík <mar.fucik@gmail.com>
2020-05-28 12:45:17 +0200

    DrawPyramid exercise

---
+++ week-02/day-04/src/Loops/Exercises/DrawPyramid.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DrawPyramid {
+  public static void main(String[] args) {
+    // Write a program that reads a number from the standard input, then draws a
+    // pyramid like this:
+    //
+    //
+    //    *
+    //   ***
+    //  *****
+    // *******
+    //
+    // The pyramid should have as many lines as the number was
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Insert integer: ");
+    int number = scanner.nextInt();
+
+    for (int i = 1; i <= number; i++) {
+      String whiteSpaces = "";
+      for ( int j = 1; j <= (number - i); j++) {
+        whiteSpaces += " ";
+      }
+
+      String stars = "";
+      for (int j = 1; j < i; j++) {
+        stars += "*";
+      }
+
+      System.out.println(whiteSpaces + stars + "*" + stars + whiteSpaces);
+    }
+  }
+}
