aa0158d62a72d8125103af1596a12d684c5f690b
Martin Fučík <mar.fucik@gmail.com>
2020-05-28 11:52:40 +0200

    MultiplicationTable exercise

---
+++ week-02/day-04/src/Loops/Exercises/MultiplicationTable.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import javafx.scene.Scene;
+
+public class MultiplicationTable {
+  public static void main(String[] args) {
+    // Create a program that asks for a number and prints the multiplication table with that number
+    //
+    // Example:
+    // The number 15 should print:
+    //
+    // 1 * 15 = 15
+    // 2 * 15 = 30
+    // 3 * 15 = 45
+    // 4 * 15 = 60
+    // 5 * 15 = 75
+    // 6 * 15 = 90
+    // 7 * 15 = 105
+    // 8 * 15 = 120
+    // 9 * 15 = 135
+    // 10 * 15 = 150
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Insert integer to create Multiplication table with: ");
+    int number = scanner.nextInt();
+
+    for (int i = 1; i <= 10; i++) {
+      int result = i * number;
+      System.out.println(i + " * " + number + " = " + result);
+    }
+  }
+}
