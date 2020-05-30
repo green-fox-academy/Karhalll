9aed794f0a4406101c8473c6e63f9548a297e2c9
Martin Fučík <mar.fucik@gmail.com>
2020-05-30 13:17:35 +0200

    DrawChesstable exercise

---
+++ week-02/day-06/src/Loops/Exercises/DrawChessTable.java
@@ -0,0 +1,29 @@
+public class DrawChessTable {
+  public static void main(String[] args) {
+    // Crate a program that draws a chess table like this
+    //
+    // % % % %
+    //  % % % %
+    // % % % %
+    //  % % % %
+    // % % % %
+    //  % % % %
+    // % % % %
+    //  % % % %
+    //
+
+    int boardSize = 8;
+
+    for (int i = 0; i < boardSize; i++) {
+      String line = "";
+      for (int j = 0; j < boardSize/2; j++) {
+        if (i % 2 == 0) {
+          line += "% ";
+        } else {
+          line += " %";
+        }
+      }
+      System.out.println(line);
+    }
+  }
+}
