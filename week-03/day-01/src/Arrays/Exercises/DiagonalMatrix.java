47d133e461dc5a37b16811c0aa038c0e40bed5ad
Martin Fučík <mar.fucik@gmail.com>
2020-06-01 18:16:57 +0200

    DiagonalMatrix exercise

---
+++ week-03/day-01/src/Arrays/Exercises/DiagonalMatrix.java
@@ -0,0 +1,34 @@
+public class DiagonalMatrix {
+  public static void main(String[] args) {
+    // - Create (dynamically) a two dimensional array
+    //   with the following matrix. Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    // - Print this two dimensional array to the output
+
+    int lines = 4;
+    int colums = 4;
+    int[][] matrix = new int[lines][colums];
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (i == j) {
+          matrix[i][j] = 1;
+        } else {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+
+    for (int[] line:matrix) {
+      for (int number:line) {
+        System.out.print(number);
+      }
+      System.out.println();
+    }
+  }
+}
