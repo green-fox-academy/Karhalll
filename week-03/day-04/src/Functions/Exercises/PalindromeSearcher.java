b1a8465a88816e6e1332b483b4c9d33be1a23226
Martin Fučík <mar.fucik@gmail.com>
2020-06-04 21:09:37 +0200

    PalindromeSearcher exercise

---
+++ week-03/day-04/src/Functions/Exercises/PalindromeSearcher.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class PalindromeSearcher {
+  public static void main(String[] args) {
+    System.out.println(palindromeSearcher("dog goat dad duck doodle never"));
+    System.out.println(palindromeSearcher("apple"));
+    System.out.println(palindromeSearcher("racecar"));
+    System.out.println(palindromeSearcher(""));
+  }
+
+  public static String palindromeSearcher(String string) {
+    ArrayList<String> palindromes = new ArrayList<String>();
+    for (int i = 0; i < string.length(); i++) {
+      for (int j = i + 1; j <= string.length(); j++) {
+        if (isPalindrome(string.substring(i, j)) && string.substring(i, j).length() >= 3) {
+          palindromes.add(string.substring(i, j));
+        }
+      }
+    }
+    return palindromes.toString();
+  }
+
+  public static boolean isPalindrome(String input) {
+    StringBuilder plain = new StringBuilder(input);
+    StringBuilder reverse = plain.reverse();
+    return (reverse.toString()).equals(input);
+  }
+}
