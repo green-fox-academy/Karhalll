957b94b7d6e373c4cbe1669590d19c040dde2da4
Martin Fučík <mar.fucik@gmail.com>
2020-06-17 16:24:48 +0200

    CountLines practise

---
+++ week-05/day-03/src/ReadingFiles/CountLines.java
@@ -0,0 +1,27 @@
+package ReadingFiles;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLines {
+  public static void main(String[] args) {
+    // Write a function that takes a filename as string,
+    // then returns the number of lines the file contains.
+    // It should return zero if it can't open the file, and
+    // should not raise any error.
+    System.out.println(countLines("countLineFile"));
+  }
+
+  public static int countLines(String fileName) {
+    try {
+      Path filePath = Paths.get("assets/" + fileName + ".txt");
+      List<String> lines = Files.readAllLines(filePath);
+      return lines.size();
+    } catch (IOException e) {
+      return 0;
+    }
+  }
+}
