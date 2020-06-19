05e430c03d6a0803f5da8169b84f96c017d4107c
Martin Fučík <mar.fucik@gmail.com>
2020-06-19 16:35:30 +0200

    TeacherStudent exercise

---
+++ week-05/day-05/src/TeacherStudent/TeacherStudent.java
@@ -0,0 +1,11 @@
+package TeacherStudent;
+
+public class TeacherStudent {
+  public static void main(String[] args) {
+    Student student = new Student();
+    Teacher teacher = new Teacher();
+
+    student.question(teacher);
+    teacher.teach(student);
+  }
+}
