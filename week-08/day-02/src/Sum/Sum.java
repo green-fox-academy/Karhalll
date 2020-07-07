6b8e84168a7d524e447b0a6763664f428e5871a0
Martin Fučík <mar.fucik@gmail.com>
2020-07-07 21:06:03 +0200

    Sum exercise

---
+++ week-08/day-02/src/Sum/Sum.java
@@ -0,0 +1,13 @@
+package Sum;
+
+import java.util.ArrayList;
+
+public class Sum {
+  public int sum(ArrayList<Integer> numbers) {
+    int sum = 0;
+    for (int number: numbers) {
+      sum += number;
+    }
+    return sum;
+  }
+}
---
+++ week-08/day-02/src/Sum/SumTest.java
@@ -0,0 +1,41 @@
+package Sum;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+class SumTest {
+  Sum sum = new Sum();
+  ArrayList<Integer> numbers = new ArrayList<>();
+
+  @Test
+  void sumList() {
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+    numbers.add(4);
+    numbers.add(5);
+
+    assertEquals(15, sum.sum(numbers));
+  }
+
+  @Test
+  void sumEmptyList() {
+    assertEquals(0, sum.sum(numbers));
+  }
+
+  @Test
+  void sumOneElementList() {
+    numbers.add(5);
+
+    assertEquals(5, sum.sum(numbers));
+  }
+
+  @Test
+  void sumNullList() {
+    ArrayList<Integer> nullList = null;
+
+    assertEquals(0, sum.sum(numbers));
+  }
+}
