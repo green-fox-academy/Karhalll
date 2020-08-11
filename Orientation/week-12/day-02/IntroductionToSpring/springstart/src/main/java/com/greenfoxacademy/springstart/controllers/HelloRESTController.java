18a33f8c53bec3343726a4a1fe42475b6fc26942
Martin Fučík <mar.fucik@gmail.com>
2020-08-11 22:11:43 +0200

    Introduction To Spring - Hello World Web App completed

--- Orientation/week-12/day-02/IntroductionToSpring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ Orientation/week-12/day-02/IntroductionToSpring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,7 +1,6 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import com.greenfoxacademy.springstart.Greeting;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
