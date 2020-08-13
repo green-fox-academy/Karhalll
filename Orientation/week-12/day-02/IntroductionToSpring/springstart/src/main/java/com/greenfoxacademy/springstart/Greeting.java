00d52552006040b83928f10c4e7d157043612c71
Martin Fučík <mar.fucik@gmail.com>
2020-08-13 18:08:37 +0200

    Say Hello to all the world (Web app) completed

--- Orientation/week-12/day-02/IntroductionToSpring/springstart/src/main/java/com/greenfoxacademy/springstart/Greeting.java
+++ Orientation/week-12/day-02/IntroductionToSpring/springstart/src/main/java/com/greenfoxacademy/springstart/Greeting.java
@@ -1,22 +1,27 @@
 package com.greenfoxacademy.springstart;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Greeting {
-  private static AtomicLong greetCount = new AtomicLong(0);
   private String content;
+  private String greeting;
+
+  private static AtomicLong greetCount = new AtomicLong(0);
+
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+  public Greeting() {
+    greetCount.addAndGet(1);
+    setGreeting();
+  }
 
   public Greeting(String content) {
-    greetCount.addAndGet(1);
     this.content = content;
-  }
-
-  public AtomicLong getGreetCount() {
-    return greetCount;
-  }
-
-  public void setGreetCount(AtomicLong greetCount) {
-    Greeting.greetCount = greetCount;
+    greetCount.addAndGet(1);
   }
 
   public String getContent() {
@@ -25,5 +30,18 @@ public class Greeting {
 
   public void setContent(String content) {
     this.content = content;
+  }
+
+  public String getGreeting() {
+    return greeting;
+  }
+
+  public void setGreeting() {
+    Random random = new Random();
+    greeting = hellos[random.nextInt(hellos.length)];
+  }
+
+  public AtomicLong getGreetCount() {
+    return greetCount;
   }
 }
--- Orientation/week-12/day-02/IntroductionToSpring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ Orientation/week-12/day-02/IntroductionToSpring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,15 +1,26 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
 
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", "World");
+  public String greeting(Model model, @RequestParam String name, @RequestParam String size,
+                         @RequestParam String r, @RequestParam String g, @RequestParam String b) {
+    Greeting greeting = new Greeting();
+
+    String style = "font-size:" + size + "px;" +
+        "color:rgb(" + r + "," + g + "," + b + ");";
+
+    model.addAttribute("greeting", greeting.getGreeting());
+    model.addAttribute("style", style);
+    model.addAttribute("name", name);
+    model.addAttribute("greetCount", greeting.getGreetCount());
     return "greeting";
   }
 }
