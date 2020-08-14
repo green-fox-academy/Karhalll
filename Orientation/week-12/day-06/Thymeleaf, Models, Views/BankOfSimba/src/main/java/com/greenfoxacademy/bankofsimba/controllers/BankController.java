834e126e039a2e5f448e61e2eba3f0902376a341
Martin Fučík <mar.fucik@gmail.com>
2020-08-14 10:24:22 +0200

    Bank of Simba - Filling multiple Accounts - completed

--- Orientation/week-12/day-06/Thymeleaf, Models, Views/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankController.java
+++ Orientation/week-12/day-06/Thymeleaf, Models, Views/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankController.java
@@ -1,6 +1,8 @@
 package com.greenfoxacademy.bankofsimba.controllers;
 
 import com.greenfoxacademy.bankofsimba.models.BankAccount;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +10,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BankController {
 
+  List<BankAccount> accounts = new ArrayList<>();
+
+  private BankController() {
+    accounts.add(new BankAccount("Simba", 2000f, "lion"));
+    accounts.add(new BankAccount("Nala", 3000f, "lion"));
+    accounts.add(new BankAccount("Mufasa", 15000f, "lion"));
+    accounts.add(new BankAccount("Timon", 10f, "meerkat"));
+    accounts.add(new BankAccount("Pumbaa", 0f, "warthog"));
+  }
+
   @GetMapping("/show")
   public String showAccount(Model model) {
     BankAccount account = new BankAccount("Simba", 2000f, "lion");
     model.addAttribute("account", account);
     return "account";
+  }
+
+  @GetMapping("/showAll")
+  public String showAllAccounts(Model model) {
+    model.addAttribute("accounts", accounts);
+    return "accounts";
   }
 
   @GetMapping("/HTMLception")
