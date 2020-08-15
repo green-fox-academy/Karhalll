05ceaf21dbf1fa673b03ac65e6577ed20bbeb205
Martin Fučík <mar.fucik@gmail.com>
2020-08-15 14:51:28 +0200

    Basic Webshop project - GET /only-available completed

--- Orientation/week-12/day-06/BasicWebshop/src/main/java/com/greenfoxacademy/basicwebshop/controllers/WebshopController.java
+++ Orientation/week-12/day-06/BasicWebshop/src/main/java/com/greenfoxacademy/basicwebshop/controllers/WebshopController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.basicwebshop.controllers;
 
 import com.greenfoxacademy.basicwebshop.structures.ShopItem;
+import com.greenfoxacademy.basicwebshop.structures.Stock;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Controller;
@@ -10,20 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebshopController {
 
-  private List<ShopItem> items;
-
-  public WebshopController() {
-    items = new ArrayList<>();
-    items.add(new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000f, 5));
-    items.add(new ShopItem("Printer", "Some HP printer that will print pages", 3000f, 2));
-    items.add(new ShopItem("Coca cola", "0.5l standard coke", 25f, 0));
-    items.add(new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119f, 100));
-    items.add(new ShopItem("T-shirt", "Blue with a corgi on a bike", 300f, 1));
-  }
+  private Stock items = new Stock();
 
   @GetMapping("/webshop")
   public String mainPage(Model model) {
-    model.addAttribute("items", items);
+    model.addAttribute("items", items.getItems());
+    return "index";
+  }
+
+  @GetMapping("/only-available")
+  public String onlyAvailable(Model model) {
+    model.addAttribute("items", items.getAvailable());
     return "index";
   }
 }
---
+++ Orientation/week-12/day-06/BasicWebshop/src/main/java/com/greenfoxacademy/basicwebshop/structures/Stock.java
@@ -0,0 +1,33 @@
+package com.greenfoxacademy.basicwebshop.structures;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stock {
+
+  private List<ShopItem> items;
+
+  public Stock() {
+    items = new ArrayList<>();
+    items.add(new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000f, 5));
+    items.add(new ShopItem("Printer", "Some HP printer that will print pages", 3000f, 2));
+    items.add(new ShopItem("Coca cola", "0.5l standard coke", 25f, 0));
+    items.add(new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119f, 100));
+    items.add(new ShopItem("T-shirt", "Blue with a corgi on a bike", 300f, 1));
+  }
+
+  public List<ShopItem> getItems() {
+    return items;
+  }
+
+  public void setItems(List<ShopItem> items) {
+    this.items = items;
+  }
+
+  public List<ShopItem> getAvailable() {
+    return items.stream()
+        .filter(i -> i.getQuantity() > 0)
+        .collect(Collectors.toList());
+  }
+}
