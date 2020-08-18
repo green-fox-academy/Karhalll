de0ad6c31e0eb5ea8117d456b41125a91e55dd7a
Martin Fučík <mar.fucik@gmail.com>
2020-08-18 14:42:02 +0200

    My Webshop project - some minor change before workshop

--- Orientation/week-12/day-06/BasicWebshop/src/main/java/com/greenfoxacademy/basicwebshop/BasicwebshopApplication.java
+++ Orientation/week-12/day-06/BasicWebshop/src/main/java/com/greenfoxacademy/basicwebshop/BasicwebshopApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicwebshopApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(BasicwebshopApplication.class, args);
   }
-
 }
