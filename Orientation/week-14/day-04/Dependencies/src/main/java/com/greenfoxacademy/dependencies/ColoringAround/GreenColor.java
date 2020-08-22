a7376a5e16531b1da8ff47994976c075ea02274b
Martin Fučík <mar.fucik@gmail.com>
2020-08-22 19:55:55 +0200

    Dependencies - Create project plus Hello ID project finished

---
+++ Orientation/week-14/day-04/Dependencies/src/main/java/com/greenfoxacademy/dependencies/ColoringAround/GreenColor.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.dependencies.ColoringAround;
+
+public class GreenColor implements MyColor {
+  @Override
+  public void printColor() {
+    System.out.println("It is green in color...");
+  }
+}
---
+++ Orientation/week-14/day-04/Dependencies/src/main/java/com/greenfoxacademy/dependencies/HelloDIProject/HelloBeanWorldApplication.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.dependencies.HelloDIProject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBeanWorldApplication implements CommandLineRunner {
+
+  @Autowired
+  private Printer printer;
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloBeanWorldApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("hello");
+  }
+}
