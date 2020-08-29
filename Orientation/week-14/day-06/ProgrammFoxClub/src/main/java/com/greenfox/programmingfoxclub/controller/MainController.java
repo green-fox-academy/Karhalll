3fcf06b6600dfe6e4ce73f826818fa6879ed0efb
Karhal <mar.fucik@gmail.com>
2020-08-29 11:00:52 +0200

    Programming Fox Club - Login - completed without optional tasks

--- Orientation/week-14/day-06/ProgrammFoxClub/src/main/java/com/greenfox/programmingfoxclub/controller/MainController.java
+++ Orientation/week-14/day-06/ProgrammFoxClub/src/main/java/com/greenfox/programmingfoxclub/controller/MainController.java
@@ -2,11 +2,24 @@ package com.greenfox.programmingfoxclub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
   @GetMapping("/")
-  public String index() {
-    return "index";
+  public String index(@RequestParam String name) {
+    return "information";
+  }
+
+  @GetMapping("/login")
+  public String login() {
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String loginAs(@RequestParam String name) {
+    return "redirect:/?name=" + name;
   }
 }
