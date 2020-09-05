d3e1bb96a00e9639900ebbbdf336ac459d3edd45
Martin Fučík <mar.fucik@gmail.com>
2020-09-05 13:09:41 +0200

    Spring + SQL - Listing todos from MySQL - completed

--- Orientation/week-15/day-06/Spring+SQL/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/TodoController.java
+++ Orientation/week-15/day-06/Spring+SQL/ConnectionWithMySQL/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/TodoController.java
@@ -1,10 +1,14 @@
 package com.greenfoxacademy.connectionwithmysql.controllers;
 
+import com.greenfoxacademy.connectionwithmysql.models.Todo;
 import com.greenfoxacademy.connectionwithmysql.repositories.TodoRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/todo")
@@ -18,8 +22,17 @@ public class TodoController {
   }
 
   @GetMapping({"/", "/list"})
-  public String list(Model model) {
-    model.addAttribute("todos", todoRepository.findAll());
+  public String list(@RequestParam(required = false) Boolean isActive, Model model) {
+    List<Todo> todos;
+    if (isActive == null) {
+      todos = todoRepository.findAll();
+    } else {
+      todos = todoRepository.findAll().stream()
+          .filter(todo -> !todo.isDone() == isActive)
+          .collect(Collectors.toList());
+      System.out.println("isActive");
+    }
+    model.addAttribute("todos", todos);
     return "todolist";
   }
 }
