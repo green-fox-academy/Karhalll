48eecc5b2049be6ba94c2648a95a6cd7a148cd5a
Martin Fučík <mar.fucik@gmail.com>
2020-09-12 09:32:25 +0200

    The Reddit Project - created Post entitiy

---
+++ Orientation/week-16/day-06/TheReddit/src/main/java/com/greenfoxacademy/reddit/model/Post.java
@@ -0,0 +1,59 @@
+package com.greenfoxacademy.reddit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String title;
+  private String url;
+  private int score = 0;
+
+  public Post() {
+  }
+
+  public Post(String title, String url) {
+    this.title = title;
+    this.url = url;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+}
