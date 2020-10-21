2d54a51207e7131346f255fd30e7434ef8607965
Martin Fučík <mar.fucik@gmail.com>
2020-10-21 20:14:45 +0200

    Change indentation for javascript files

--- Project/week-21/day-03/arrays.js
+++ Project/week-21/day-03/arrays.js
@@ -1,17 +1,17 @@
 function createShallowCopy(array) {
-    return [...array];
+  return [...array];
 }
 
 function areEquale(array1, array2) {
-    if (array1.length == array2.length) {
-        for (let i = 0; i < array1.length; i++) {
-            if (array1[i] === array2[i]) {
-                return true;
-            }
-        }
-    } else { 
-        return false; 
+  if (array1.length == array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        return true;
+      }
     }
+  } else { 
+    return false; 
+  }
 }
 
 const testArray = [1, 2, 3];
