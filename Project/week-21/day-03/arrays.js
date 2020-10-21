71d8f2e569475afcef4e5fba6b5bfcf3091d1e96
Martin Fučík <mar.fucik@gmail.com>
2020-10-21 20:11:53 +0200

    Completed Array exercise

---
+++ Project/week-21/day-03/arrays.js
@@ -0,0 +1,19 @@
+function createShallowCopy(array) {
+    return [...array];
+}
+
+function areEquale(array1, array2) {
+    if (array1.length == array2.length) {
+        for (let i = 0; i < array1.length; i++) {
+            if (array1[i] === array2[i]) {
+                return true;
+            }
+        }
+    } else { 
+        return false; 
+    }
+}
+
+const testArray = [1, 2, 3];
+const shallowCoppy = createShallowCopy(testArray);
+console.log(areEquale(testArray, shallowCoppy));
