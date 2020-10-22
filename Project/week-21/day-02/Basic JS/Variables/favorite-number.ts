5a054be4a7c38db7bb5b3f77c9af6c1e0edc4b63
Martin Fučík <mar.fucik@gmail.com>
2020-10-22 16:22:01 +0200

    Variables favorite-number completed

---
+++ Project/week-21/day-02/Basic JS/Variables/favorite-number.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let favoriteNumber: number = 8
+
+// Store your favorite number in a variable (as a number)
+let myFaviriteNumber: number = 8;
+
+// And print it like this: 'My favorite number is: 8'
+console.log(`My favorite number is: ${myFaviriteNumber}`);
