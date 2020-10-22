7e3430c78194e00dcadce8c6bf72315f4a71b740
Martin Fučík <mar.fucik@gmail.com>
2020-10-22 16:34:58 +0200

    Basic JS - Variables define-basic-info completed

---
+++ Project/week-21/day-02/Basic JS/Variables/define-basic-info.js
@@ -0,0 +1,14 @@
+'use strict';
+// Define several things as a variable, then print their values
+// Your name as a string
+var myName = 'Martin Fučík';
+console.log(myName);
+// Your age as a number
+var myAge = 30;
+console.log(myAge);
+// Your height in meters as a number
+var myHeight = 1.95;
+console.log(myHeight);
+// Whether you are married or not as a boolean
+var amIMarried = false;
+console.log(amIMarried);
---
+++ Project/week-21/day-02/Basic JS/Variables/define-basic-info.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+let myName: string = 'Martin Fučík';
+console.log(myName);
+
+// Your age as a number
+let myAge: number = 30;
+console.log(myAge);
+
+// Your height in meters as a number
+let myHeight: number = 1.95;
+console.log(myHeight);
+
+// Whether you are married or not as a boolean
+let amIMarried: boolean = false;
+console.log(amIMarried);
