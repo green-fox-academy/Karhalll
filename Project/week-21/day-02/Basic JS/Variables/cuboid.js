31b92653389604f050dbb238fd212009646b5bb7
Martin Fučík <mar.fucik@gmail.com>
2020-10-22 16:50:09 +0200

    Basic JS - Variables cuboid completed

---
+++ Project/week-21/day-02/Basic JS/Variables/cuboid.js
@@ -0,0 +1,11 @@
+'use strict';
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+var sideA = 2;
+var sideB = 3;
+var sideC = 4;
+console.log('Surface Area: ' + 2 * (sideA * sideB + sideA * sideC + sideB * sideC));
+console.log('Volume: ' + sideA * sideB * sideC);
