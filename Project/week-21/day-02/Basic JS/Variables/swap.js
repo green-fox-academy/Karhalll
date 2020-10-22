276c60c0c64fa14f4b553c72e8bada2240850135
Martin Fučík <mar.fucik@gmail.com>
2020-10-22 16:24:30 +0200

    Variables swap completed

--- Project/week-21/day-02/Basic JS/Variables/swap.js
+++ Project/week-21/day-02/Basic JS/Variables/swap.js
@@ -1,12 +1,9 @@
 'use strict';
-
 // Swap the values of these variables
-let numberToSwap1: number = 123;
-let numberToSwap2: number = 526;
-
-let numberHolder: number = numberToSwap1;
+var numberToSwap1 = 123;
+var numberToSwap2 = 526;
+var numberHolder = numberToSwap1;
 numberToSwap1 = numberToSwap2;
 numberToSwap2 = numberHolder;
-
 console.log(numberToSwap1);
 console.log(numberToSwap2);
