328e94fa75bdd487fda1acaf0d77665a72172069
Martin Fučík <mar.fucik@gmail.com>
2020-10-23 14:10:53 +0200

    Data Structures - Data Structures list-introduction completed

---
+++ Project/week-21/day-04/DataStructures/DataStructures/list-introduction.js
@@ -0,0 +1,13 @@
+var listOfNames = new Array();
+console.log(listOfNames.length);
+listOfNames.push('William');
+console.log(listOfNames.length > 0);
+listOfNames.push('John');
+listOfNames.push('Amanda');
+console.log(listOfNames.length);
+console.log(listOfNames[2]);
+listOfNames.forEach(function (name) { return console.log(name); });
+listOfNames.forEach(function (name, index) { return console.log(index + 1 + ". " + name); });
+listOfNames.splice(3, 1);
+listOfNames.reverse().forEach(function (name) { return console.log(name); });
+listOfNames.splice(0, listOfNames.length);
